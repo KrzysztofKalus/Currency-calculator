b42b3af8629a27d1655e5a44f1f1957cc304c801
Krzysztof Kalus <eumo37@gmail.com>
2023-04-14 17:14:47 +0200

    transform to function

--- js/script.js
+++ js/script.js
@@ -1,33 +1,45 @@
-console.log("Cześć");
+const welcome = () => {
+    console.log("Cześć, miło mi, że tu zaglądasz");
+}
+welcome();
 
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
 
-let rateEURO = 4.6810;
-let rateUSD = 4.2917;
-let rateGBP = 5.3369;
 
-formElement.addEventListener("click", (event) => {
-    event.preventDefault();
+{
+    const formElement = document.querySelector(".js-form");
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+    const resultElement = document.querySelector(".js-result");
 
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
-    let result;
+    const rateEURO = 4.6810;
+    const rateUSD = 4.2917;
+    const rateGBP = 5.3369;
 
-    switch (currency) {
-        case "EURO":
-            result = amount / rateEURO;
-            break;
+    const onValueCurency = (event) => {
+        event.preventDefault();
 
-        case "USD":
-            result = amount / rateUSD;
-            break;
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+        let result;
 
-        case "GBP":
-            result = amount / rateGBP;
-            break;
+        switch (currency) {
+            case "EURO":
+                result = amount / rateEURO;
+                break;
+
+            case "USD":
+                result = amount / rateUSD;
+                break;
+
+            case "GBP":
+                result = amount / rateGBP;
+                break;
+        }
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency} </strong>`;
     }
-    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency} </strong>`;
-})
+
+    const init = () => {
+        formElement.addEventListener("click", onValueCurency);
+    }
+    init();
+}
